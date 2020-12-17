c29add5f99221b8e7e3116eaba1c4fda715e613c
Bohdana3007 <bohdanakushniruk1@gmail.com>
2020-12-17 13:31:45 +0200

    add homework

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+
+import s from "./FriendList.module.css";
+import FriendListItem from "./FriendListItem";
+
+function FriendList({ friends }) {
+  return (
+    <ul className={s.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+}
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      isOnline: PropTypes.bool,
+    })
+  ),
+};
+
+export default FriendList;
